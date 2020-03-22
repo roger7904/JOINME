43476e40ab2e4bc1eb037167d1c696af5f6e2191
roger7904 <a1063333@mail.nuk.edu.tw>
2020-03-22 21:17:10 +0800

    main update

--- app/src/main/java/com/roger/joinme/MainActivity.java
+++ app/src/main/java/com/roger/joinme/MainActivity.java
@@ -2,16 +2,63 @@ package com.roger.joinme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button login;
+    private Button register;
+    private Button forgetpwd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //push test1
-        //push test2
-        // push3
+
+        initViews();
+        initData();
+        setListeners();
+    }
+
+    private void initViews()
+    {
+        login=(Button)findViewById(R.id.login);
+        register=(Button)findViewById(R.id.register);
+        forgetpwd=(Button)findViewById(R.id.forgetpassword);
+    }
+
+    private void initData()
+    {
+    }
+
+    private void setListeners()
+    {
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,homepage.class);
+                startActivity(intent);
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,register.class);
+                startActivity(intent);
+            }
+        });
+        forgetpwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,forgetpwd.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
