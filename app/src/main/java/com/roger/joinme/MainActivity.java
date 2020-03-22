3aac9d8b469f77c3e09c7073bcea88e58eac74c7
roger7904 <a1063333@mail.nuk.edu.tw>
2020-03-22 14:44:42 +0800

    add bottom_bar

--- app/src/main/java/com/roger/joinme/MainActivity.java
+++ app/src/main/java/com/roger/joinme/MainActivity.java
@@ -2,16 +2,50 @@ package com.roger.joinme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button login;
+    private Button register;
+    private Button forgetpw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //push test1
-        //push test2
-        // push3
+
+        //初始化控制項
+        initViews();
+        //初始化資料
+        initData();
+        //設定監聽事件
+        setListeners();
+    }
+
+    private void initViews()
+    {
+        login=(Button)findViewById(R.id.login);
+        register=(Button)findViewById(R.id.register);
+        forgetpw=(Button)findViewById(R.id.forgetpassword);
+    }
+
+    private void initData()
+    {
+    }
+
+    private void setListeners()
+    {
+        Button login=(Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,homepage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
