acfda5db50fa8c6c6e47cc3bce5a4c1f6c00a1b7
tzuyen <angela19990807@gmail.com>
2020-03-22 21:10:11 +0800

    test

--- app/src/main/java/com/roger/joinme/MainActivity.java
+++ app/src/main/java/com/roger/joinme/MainActivity.java
@@ -2,63 +2,16 @@ package com.roger.joinme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button login;
-    private Button register;
-    private Button forgetpwd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initViews();
-        initData();
-        setListeners();
-    }
-
-    private void initViews()
-    {
-        login=(Button)findViewById(R.id.login);
-        register=(Button)findViewById(R.id.register);
-        forgetpwd=(Button)findViewById(R.id.forgetpassword);
-    }
-
-    private void initData()
-    {
-    }
-
-    private void setListeners()
-    {
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this,homepage.class);
-                startActivity(intent);
-            }
-        });
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this,register.class);
-                startActivity(intent);
-            }
-        });
-        forgetpwd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this,forgetpwd.class);
-                startActivity(intent);
-            }
-        });
-
+        //push test1
+        //push test2
+        // push3
     }
 }
