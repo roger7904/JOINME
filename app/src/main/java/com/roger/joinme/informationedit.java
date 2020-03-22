81c0212dad89503345b591b4c330a9079d6ce435
roger7904 <a1063333@mail.nuk.edu.tw>
2020-03-22 21:08:06 +0800

    some problems happened

---
+++ app/src/main/java/com/roger/joinme/informationedit.java
@@ -0,0 +1,39 @@
+package com.roger.joinme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class informationedit extends AppCompatActivity {
+
+    private Button send;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informationedit);
+
+        initViews();
+        setListeners();
+    }
+
+    private void initViews()
+    {
+        send=(Button)findViewById(R.id.btn_send);
+    }
+
+
+    private void setListeners() {
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(informationedit.this, homepage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
