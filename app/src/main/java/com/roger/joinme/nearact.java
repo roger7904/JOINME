03d4f30f3c48c945f13c3ac5a89607cef8c98b31
tzuyen <angela19990807@gmail.com>
2020-03-24 00:41:22 +0800

    can't test on computer QQ

--- app/src/main/java/com/roger/joinme/nearact.java
+++ app/src/main/java/com/roger/joinme/nearact.java
@@ -13,7 +13,7 @@ public class nearact extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_nearact);
 
         initViews();
         setListeners();
