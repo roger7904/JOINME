790d26740553fe284fa9a5afe9b55cf59bde21cf
chouchinlin <52076113+chouchinlin@users.noreply.github.com>
2020-06-06 17:47:09 +0800

    創立活動的選取日期跟時間完成 剩下上傳活動封面(應該是用firebase storage)跟上傳資料庫要做

--- app/src/main/java/com/roger/joinme/jo.java
+++ app/src/main/java/com/roger/joinme/jo.java
@@ -1,11 +1,17 @@
 package com.roger.joinme;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,6 +20,8 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import java.util.Calendar;
 
 public class jo extends AppCompatActivity {
     private Button user;
@@ -28,6 +36,11 @@ public class jo extends AppCompatActivity {
     private ImageButton notice;
     private ImageButton setting;
     private AppBarConfiguration mAppBarConfiguration;
+    private Spinner spinner;
+    private Button startBtn, endBtn, dateBtn;
+    private int year, month, day; //選擇日期變數
+    private int sHour,sMin,eHour,eMin;  //起訖時間變數
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +69,10 @@ public class jo extends AppCompatActivity {
         //NavigationUI.setupWithNavController(navigationView, navController);
 
         initViews();
-//        setListeners();
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, new String[]{"商家優惠", "球類", "限時", "KTV", "其他"});
+        setListeners();
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     @Override
@@ -72,27 +88,29 @@ public class jo extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
-    private void initViews()
-    {
-        user=(Button)findViewById(R.id.btn_user);
-        homepage=(Button)findViewById(R.id.btn_to_homepage);
-        selfpage=(Button)findViewById(R.id.btn_to_selfpage);
-        activitypage=(Button)findViewById(R.id.btn_to_jo);
-        friendpage=(Button)findViewById(R.id.btn_to_notice);
-        logout=(Button)findViewById(R.id.btn_logout);
-        chatroom=(ImageButton)findViewById(R.id.imgbtn_chatroom);
-        favorite=(ImageButton)findViewById(R.id.imgbtn_favorite);
-        jo=(ImageButton)findViewById(R.id.imgbtn_jo);
-        notice=(ImageButton)findViewById(R.id.imgbtn_notice);
-        setting=(ImageButton)findViewById(R.id.imgbtn_setting);
+
+    private void initViews() {
+        user = (Button) findViewById(R.id.btn_user);
+        homepage = (Button) findViewById(R.id.btn_to_homepage);
+        selfpage = (Button) findViewById(R.id.btn_to_selfpage);
+        activitypage = (Button) findViewById(R.id.btn_to_jo);
+        friendpage = (Button) findViewById(R.id.btn_to_notice);
+        logout = (Button) findViewById(R.id.btn_logout);
+        chatroom = (ImageButton) findViewById(R.id.imgbtn_chatroom);
+        favorite = (ImageButton) findViewById(R.id.imgbtn_favorite);
+        jo = (ImageButton) findViewById(R.id.imgbtn_jo);
+        notice = (ImageButton) findViewById(R.id.imgbtn_notice);
+        setting = (ImageButton) findViewById(R.id.imgbtn_setting);
+        spinner = (Spinner) findViewById(R.id.activityType);
+        dateBtn = (Button) findViewById(R.id.dateBtn);
+        endBtn = (Button) findViewById(R.id.endBtn);
+        startBtn = (Button) findViewById(R.id.startBtn);
     }
 
-    private void initData()
-    {
+    private void initData() {
     }
 
-    private void setListeners()
-    {
+    private void setListeners() {
         user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -166,34 +184,101 @@ public class jo extends AppCompatActivity {
 //        });
 
 
+//        jo.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                intent.setClass(jo.this, jo.class);
+//                startActivity(intent);
+//            }
+//        });
 
-        jo.setOnClickListener(new View.OnClickListener() {
+//        notice.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                intent.setClass(jo.this, notice.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        setting.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                intent.setClass(jo.this, setting.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        dateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(jo.this, jo.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                day = c.get(Calendar.DAY_OF_MONTH);
+                new DatePickerDialog(jo.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        dateBtn.setText(setDateFormat(year, month, day));
+                    }
+
+                }, year, month, day).show();
             }
+
         });
 
-        notice.setOnClickListener(new View.OnClickListener() {
+        startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(jo.this, notice.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                sHour = c.get(Calendar.HOUR_OF_DAY);
+                sMin = c.get(Calendar.MINUTE);
+                new TimePickerDialog(jo.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int Hour, int Min) {
+                        startBtn.setText(setTimeFormat(Hour,Min));
+                        sHour=Hour;
+                        sMin=Min;
+//                        System.out.println(Hour+":"+Min);
+                    }
+                }, sHour,sMin,false).show();
+
             }
+
         });
 
-        setting.setOnClickListener(new View.OnClickListener() {
+        endBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(jo.this, setting.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                eHour = c.get(Calendar.HOUR_OF_DAY);
+                eMin = c.get(Calendar.MINUTE);
+                new TimePickerDialog(jo.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int Hour, int Min) {
+                        endBtn.setText(setTimeFormat(Hour,Min));
+                        eHour=Hour;
+                        eMin=Min;
+                    }
+                }, eHour,eMin,false).show();
             }
-        });
 
+        });
+    }
+
+    private String setDateFormat(int year, int monthOfYear, int dayOfMonth) {
+        return String.valueOf(year) + "-"
+                + String.valueOf(monthOfYear + 1) + "-"
+                + String.valueOf(dayOfMonth);
+    }
+
+    private String setTimeFormat(int hr,int min) {
+        return String.valueOf(hr)+":"+String.valueOf(min);
     }
 }
+
+
+
 
