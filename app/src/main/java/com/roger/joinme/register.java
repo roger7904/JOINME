37689f78994d4fe8cdcb909659df9a0a4da1c834
roger <a1063333@mail.nuk.edu.tw>
2020-07-11 22:05:51 +0800

    註冊授權完成，還沒抓錯誤訊息告知使用者未完成註冊原因(先不要用註冊的帳號登入，有用加密，還要改登入的方式)

--- app/src/main/java/com/roger/joinme/register.java
+++ app/src/main/java/com/roger/joinme/register.java
@@ -1,30 +1,51 @@
 package com.roger.joinme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class register extends AppCompatActivity {
 
     private Button back_to_login;
-    private EditText editText_email;
-    private EditText editText_pwd;
+    private EditText UserEmail;
+    private EditText UserPassword;
+    private Button CreateAccountButton;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
         initViews();
         setListeners();
 
@@ -48,9 +69,11 @@ public class register extends AppCompatActivity {
 
     private void initViews()
     {
-        editText_pwd = (EditText)findViewById(R.id.editText_pwd);
-        editText_email = (EditText)findViewById(R.id.editText_email);
+        UserPassword = (EditText)findViewById(R.id.editText_pwd);
+        UserEmail = (EditText)findViewById(R.id.editText_email);
         back_to_login=(Button)findViewById(R.id.btn_back_to_login);
+        loadingBar = new ProgressDialog(this);
+        CreateAccountButton = (Button) findViewById(R.id.button_verify);
     }
 
     private void initData()
@@ -59,16 +82,93 @@ public class register extends AppCompatActivity {
 
     private void setListeners()
     {
-        String email = editText_email.getText().toString();
-        String passwd = editText_pwd.getText().toString();
-        Log.d(email,"1");
 
         back_to_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(register.this,MainActivity.class);
+                startActivity(intent);
                 register.this.finish();
             }
         });
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                CreateNewAccount();
+            }
+        });
+    }
+    private void CreateNewAccount()
+    {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Creating New Account");
+            loadingBar.setMessage("Please wait, while we wre creating new account for you...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull com.google.android.gms.tasks.Task<AuthResult> task) {
+                            if (task.isSuccessful())
+                            {
+                                String deviceToken = FirebaseInstanceId.getInstance().getToken();
+
+                                String currentUserID = mAuth.getCurrentUser().getUid();
+                                final Map<String, Object> registerdata = new HashMap<>();
+                                registerdata.put("currentUserID",currentUserID);
+                                registerdata.put("device_token",deviceToken);
+                                db.collection("user")
+                                        .document(currentUserID)
+                                        .set(registerdata)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d("TAG", "DocumentSnapshot successfully written!");
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w("TAG", "Error writing document", e);
+                                            }
+                                        });
+
+                                SendUserToMainActivity();
+                                Toast.makeText(register.this, "Account Created Successfully...", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                            else
+                            {
+                                String message = task.getException().toString();
+                                Toast.makeText(register.this, "Error : " + message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+    }
+
+
+    private void SendUserToMainActivity()
+    {
+        Intent mainIntent = new Intent(register.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 }
-//2
