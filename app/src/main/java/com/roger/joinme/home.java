9c862e0098dd5decf17d76de77a690d7c623c61c
roger <a1063333@mail.nuk.edu.tw>
2020-08-08 20:29:55 +0800

    評價介面done

--- app/src/main/java/com/roger/joinme/home.java
+++ app/src/main/java/com/roger/joinme/home.java
@@ -141,25 +141,7 @@ public class home extends AppCompatActivity implements OnMapReadyCallback, Googl
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-//        //NavigationView navigationView = findViewById(R.id.nav_view);
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        mAppBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-//                .setDrawerLayout(drawer)
-//                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        //NavigationUI.setupWithNavController(navigationView, navController);
+
         if (chechPermission()) {
             init();
         }
@@ -197,23 +179,6 @@ public class home extends AppCompatActivity implements OnMapReadyCallback, Googl
             VerifyUserExistance();
 
             currentUserID = mAuth.getCurrentUser().getUid();
-//            DocumentReference docRef = db.collection("user").document(currentUserID);
-//            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//                @Override
-//                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                    if (task.isSuccessful()) {
-//                        DocumentSnapshot document = task.getResult();
-//                        if (document.exists()) {
-//                            useraccount=document.getString("email");
-//                            Log.d("TAG", "DocumentSnapshot data: " + document.getData());
-//                        } else {
-//                            Log.d("TAG", "No such document");
-//                        }
-//                    } else {
-//                        Log.d("TAG", "get failed with ", task.getException());
-//                    }
-//                }
-//            });
         }
     }
 
