409247094e4573922846378757f70b38c1628526
angela_zy_chang <angela_zy_chang@gss.com.tw>
2020-09-17 22:49:44 +0800

    先丟個

---
+++ app/src/main/java/com/roger/joinme/personholdactAdapter.java
@@ -0,0 +1,24 @@
+package com.roger.joinme;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class personholdactAdapter extends RecyclerView.Adapter<requestAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public requestAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull requestAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
