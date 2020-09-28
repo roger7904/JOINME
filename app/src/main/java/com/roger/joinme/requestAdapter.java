500eb441036735f7a7c3f091497e39dcee360a0c
roger <a1063333@mail.nuk.edu.tw>
2020-09-28 14:51:00 +0800

    contentcount

--- app/src/main/java/com/roger/joinme/requestAdapter.java
+++ app/src/main/java/com/roger/joinme/requestAdapter.java
@@ -225,13 +225,13 @@ public class requestAdapter extends RecyclerView.Adapter<requestAdapter.ViewHold
                 Map<String, Object> receiverdata2 = new HashMap<>();
                 receiverdata2.put("newestcontent", " ");
                 receiverdata2.put("newestmillisecond", ts);
-                receiverdata2.put("contentcount","0");
+                receiverdata2.put("contentcount",0);
                 receiverdata2.put("from",request.getName());
                 Map<String, Object> senderdata2 = new HashMap<>();
                 senderdata2.put("UserID", currentUserID);
                 senderdata2.put("newestcontent", " ");
                 senderdata2.put("newestmillisecond", ts);
-                senderdata2.put("contentcount","0");
+                senderdata2.put("contentcount",0);
                 senderdata2.put("from",currentUserName);
                 db.collection("message").document(currentUserID).
                         collection("UserID").document(request.getID())
