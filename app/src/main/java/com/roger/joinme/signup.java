e1bae0e2af7bb79cc4409248082f5c892f1aa82d
chouchinlin <52076113+chouchinlin@users.noreply.github.com>
2020-10-31 16:48:05 +0800

    signup舉辦人名字出得來了

--- app/src/main/java/com/roger/joinme/signup.java
+++ app/src/main/java/com/roger/joinme/signup.java
@@ -154,16 +154,17 @@ public class signup extends AppCompatActivity {
                                         }
                                     });
                                 }
+                                System.out.println(organizerID);
 
-                                final DocumentReference docRef = db.collection("user").document(organizerID).collection("profile")
-                                        .document(organizerID);
+                                final DocumentReference docRef = db.collection("user").document(organizerID).collection("profile").document(organizerID);
                                 docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                                     @Override
                                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                                         if (task.isSuccessful()) {
                                             DocumentSnapshot snapshot = task.getResult();
-                                            if (snapshot != null && snapshot.exists()) {
+                                            if (snapshot.exists()) {
                                                 organizerName=snapshot.getString("name");
+                                                activityContent.setText("類別：" + document.getString("activityType") + "\n開始時間：" + ft.format(snnippet) + "\n結束時間：" + ft.format(snnippet2) + "\n" + "地點：" + document.getString("location") + "\n" + "備註：" + document.getString("postContent") + "\n" + "發起人：" + organizerName);
                                             } else {
 
                                             }
@@ -171,7 +172,7 @@ public class signup extends AppCompatActivity {
                                     }
                                 });
 
-                                activityContent.setText("類別：" + document.getString("activityType") + "\n開始時間：" + ft.format(snnippet) + "\n結束時間：" + ft.format(snnippet2) + "\n" + "地點：" + document.getString("location") + "\n" + "備註：" + document.getString("postContent") + "\n" + "發起人：" + organizerName);
+
                                 if (!document.getString("organizerID").equals(currentUserID)) {
                                     deletebtn.setVisibility(View.GONE);
                                 }else{
