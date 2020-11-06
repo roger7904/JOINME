83ba6b506675a4dc704e9943b997a84d46ab5b1c
angela_zy_chang <angela_zy_chang@gss.com.tw>
2020-11-06 21:05:28 +0800

    返回鍵修改fin

--- app/src/main/java/com/roger/joinme/personalevaluateActivity.java
+++ app/src/main/java/com/roger/joinme/personalevaluateActivity.java
@@ -3,8 +3,10 @@ package com.roger.joinme;
 import android.content.Intent;
 import android.net.Uri;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -267,6 +269,26 @@ public class personalevaluateActivity extends AppCompatActivity
                 }
             }
         });
+    }
+
+    //鎖手機的返回鍵
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            if(getApplicationInfo().targetSdkVersion >= Build.VERSION_CODES.ECLAIR){
+                event.startTracking();
+                Intent intent = new Intent();
+                intent.setClass(personalevaluateActivity.this, home.class);
+                startActivity(intent);
+            }else{
+                onBackPressed();
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public  boolean onKeyUp(int keyCode, KeyEvent event){
+        return super.onKeyUp(keyCode, event);
     }
 
 //    private void SendUserToMainActivity()
